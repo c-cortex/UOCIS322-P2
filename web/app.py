c1335919dc7c49b14068b5831f7f474f9f7a4493
Christopher Cortes <ccortes@testium.cs.uoregon.edu>
2021-04-25 13:15:02 -0700

    final commit for all except app.py

--- web/app.py
+++ web/app.py
@@ -1,14 +1,41 @@
 """
-John Doe's Flask API.
+Chris Cortes' Flask API.
 """
 
-from flask import Flask
+from flask import Flask, render_template, send_from_directory, abort
+
 
 app = Flask(__name__)
 
+# this is the defult webpage
 @app.route("/")
 def hello():
     return "UOCIS docker demo!\n"
+
+# reads the path entered
+@app.route("/<path:path>")
+def read_path(path):
+    # checks if path contains forbidden characters, results in error 403 if found
+    if "~" in path or ".." in path or "//" in path:
+        abort(403)
+    else:
+        # trys to open file 
+        try:
+            return send_from_directory('pages',path)
+        # will result in error 404 if no file found
+        except:
+            abort(404)
+
+# error 403, takes the error html from templates and displays it
+@app.errorhandler(403)
+def forbidden_403(e):
+    return render_template('403.html'), 403
+
+# error 404, takes the error html from templates and displays it
+@app.errorhandler(404)
+def not_found_404(e):
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
